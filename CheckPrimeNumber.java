8422f2ee4c79f1ccfd2429bcc6471950f0ddc294
Git-D <samruddhi040806@gmail.com>
2025-05-16 21:18:09 +0530

    CheckingPrimeNumberCode

---
+++ CheckPrimeNumber.java
@@ -0,0 +1,28 @@
+import java.lang.*;
+
+public class CheckPrimeNumber {
+    static boolean isPrime(int n) {
+        if (n == 1 || n == 0)
+            return false;
+
+        // Run a loop from 2 to n-1
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        // otherwise, n is prime number.
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int N = 100;
+        // check for every number from 1 to N
+        for (int i = 1; i <= N; i++) {
+            // check if current number is prime
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
